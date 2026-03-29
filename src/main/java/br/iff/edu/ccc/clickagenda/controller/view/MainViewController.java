c036067acf0d638d01f8d5803f48066f703f7e79
Jorge Pozes <jorgepozes027@gmail.com>
2026-03-29 16:31:56 -0300

    implementa layout base da página inicial

--- src/main/java/br/iff/edu/ccc/clickagenda/controller/view/MainViewController.java
+++ src/main/java/br/iff/edu/ccc/clickagenda/controller/view/MainViewController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/index")
 public class MainViewController {
 
     @GetMapping()
-    public String home() {
-        return "home";
+    public String index() {
+        return "index";
     }
 }
